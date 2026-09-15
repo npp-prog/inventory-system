57b888a3bb9b82ba7f93aafd1e49b043a06e7c66
npp-prog <npp@mgocandoniaccounting.org>
2026-09-16 01:39:04 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { watchAuth, auth } from "./firebase.js";
+import { watchAuth, auth, signOut, isAllowedAccount } from "./firebase.js";
 import { initAuthUi } from "./auth-ui.js";
 import { initApp } from "./app.js";
 
@@ -10,6 +10,17 @@ const appScreen = document.getElementById("appScreen");
 let appStarted = false;
 
 watchAuth((user) => {
+  // Optional domain guard (see allowedEmailDomains in firebase-config.js). Matters most once
+  // Google sign-in is enabled, since Firebase will otherwise accept any Google account.
+  if (user && !isAllowedAccount(user)) {
+    const errBox = document.getElementById("signinError");
+    if (errBox) {
+      errBox.textContent = `${user.email || "That account"} isn't allowed to use this system. Sign in with your municipal account.`;
+      errBox.hidden = false;
+    }
+    signOut();
+    return;
+  }
   if (user) {
     signinScreen.hidden = true;
     appScreen.hidden = false;
